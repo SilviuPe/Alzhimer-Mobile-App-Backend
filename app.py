3382694ad21006851b46a7b3b4578a79318a8ead
Silviu <silviu@servcast.net>
2025-06-05 21:28:24 +0300

    feat/new apis

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 import requests
 
+from cognitive_support import get_activity_message
+from therapy_and_recreation import create_puzzle
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,28 +13,38 @@ def home():
 
 @app.route('/cognitive-support/dashboard')
 def dashboard():
-    # API_KEY = '255a2a2498d31a425a439aa0ff887a87'
-    # CITY = "Galati"
-    # URL = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
-    # print(URL)
-    # response = requests.get(URL)
-    # data = response.json()
-
-    
-    # if response.status_code == 200:
-    #     temp = data['main']['temp']
-    #     weather = data['weather'][0]['description']
-
-    #     return { "temp" : temp, "weather" : weather}, 200
-
-    # else:
-    #     return {"Error": f"{data.get('message', 'Failed to retrieve data')}"}, 400
-
     return {
         'temp' : 20,
         'location': 'Bucharest'
     }, 200
     
+
+@app.route('/cognitive-support/ai-check-in')
+def ai_check_in():
+    return {}, 200
+
+@app.route('/cognitive-support/digital-storybook')
+def digital_storybook():
+    return {}, 200
+
+@app.route('/cognitive-support/locked-down-mode')
+def locked_down_mode():
+    return {}, 200
+
+@app.route('/cognitive-support/what-am-i-doing')
+def waid():
+
+    message = get_activity_message()
+
+    return {'message': message}, 200
+
+
+@app.route('/therapy-and-recreation/puzzle-coach')
+def puzzle_therapy():
+    
+    game = create_puzzle()
+
+    return game, 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
